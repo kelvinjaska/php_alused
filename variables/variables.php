8e354581217d3d3ed78fff66cf9c7c4e3ed9a937
kelvinjaska <kelvin.jaska@gmail.com>
2018-12-14 12:31:43 +0200

    PHP2

---
+++ variables/variables.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kelvin.jaska
+ * Date: 13.12.2018
+ * Time: 13:08
+ */
+
+// matemaatilised ja vordlusoperaatorid
+
+$arv1 = 42;
+$arv2 = 11;
+
+
+echo $arv1 . ' + ' . $arv2 . ' = ' . ($arv1 + $arv2). ' <br> ';
+echo $arv1 . ' - ' . $arv2 . ' = ' . ($arv1 - $arv2). ' <br> ';
+echo $arv1 . ' * ' . $arv2 . ' = ' . ($arv1 * $arv2). ' <br> ';
+echo $arv1 . ' / ' . $arv2 . ' = ' . ($arv1 / $arv2). ' <br> ';
+echo $arv1 . ' % ' . $arv2 . ' = ' . ($arv1 % $arv2). ' <br> ';
+echo '<hr>';
+
+echo $arv1 . ' == ' . $arv2 . ' on ' . ($arv1 == $arv2). ' <br> ';
+echo $arv1 . ' != ' . $arv2 . ' on ' . ($arv1 != $arv2). ' <br> ';
+echo $arv1 . ' <= ' . $arv2 . ' on ' . ($arv1 <= $arv2). ' <br> ';
+echo $arv1 . ' >= ' . $arv2 . ' on ' . ($arv1 >= $arv2). ' <br> ';
+echo $arv1 . ' < ' . $arv2 . ' on ' . ($arv1 < $arv2). ' <br> ';
+echo $arv1 . ' > ' . $arv2 . ' on ' . ($arv1 > $arv2). ' <br> ';
