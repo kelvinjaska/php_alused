1801a3401c2933db95b60dcea122e9488ec3ae0a
kelvinjaska <kelvin.jaska@gmail.com>
2018-12-14 12:52:06 +0200

    tigimuslaused else if

--- conditions/index.php
+++ conditions/index.php
@@ -10,14 +10,38 @@
 *  if (tingimus) {
  * tegevused , mis toimuvad siis, kui tingimus kehtib
  * } else {
- * mui,du, kui tingimus ei kehti, toimuvad siin kirjutatud tegevused
+ * muidu, kui tingimus ei kehti, toimuvad siin kirjutatud tegevused
  * }
 */
+/** tingimuslaused
+ *  if (tingimus) {
+ * tegevused , mis toimuvad siis, kui tingimus kehtib
+ * } else if (tingimus2) {
+ * muidu, kui tingimus ei kehti, toimuvad siin kirjutatud tegevused
+ * } else {
+ * muidu, kui koik mis eelenvat kirjutatud, rs sobi , tootab see osa
+ * }
+ */
 
-echo '<h5>Ulesanne</h5>';
-$arv1 = 68;
-if(arv % 2 == 0) {
-    echo '<div style="color: red";>'.$arv.'</div>';
+echo '<h5 style="font-size:50px; color: white;">Ulesanne</h5>';
+$arv = rand(0, 100);
+if($arv % 2 == 0) {
+    echo '<div style="color: red;">'. $arv .'</div>';
 } else {
     echo '<div style="color: blue;">'. $arv . '</div>';
+}
+
+echo '<hr>';
+echo '<body style="background: black">';
+$arv2 = rand(0,100);
+if($arv2 >= 0 and $arv2 < 25) {
+    echo '<div style="color: red; font-size: 40px">' . $arv2 . '</div>';
+} else if ($arv2 >= 25 and $arv2 < 50) {
+    echo '<div style="color: green; font-size: 40px">' . $arv2 . '</div>';
+} else if ($arv2 >= 50 and $arv2 < 75) {
+    echo '<div style="color: yellow; font-size: 40px">' . $arv2 . '</div>';
+} else if ($arv2 >= 75 and $arv2 < 100) {
+    echo '<div style="color: orange; font-size: 40px">' . $arv2 . '</div>';
+} else {
+    echo '<div style="color: white; font-size: 40px">' . $arv2 . '</div>';
 }
